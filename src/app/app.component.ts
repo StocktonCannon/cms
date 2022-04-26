b5b0a8f238f7e47e9c8d9d1aeb06bdbe54a48d90
Stockton Cannon <77078756+StocktonCannon@users.noreply.github.com>
2022-04-26 16:34:47 -0600

    Week2 Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'cms-root',
+  // template: '<h1>Hello World!</h1>',
   templateUrl: './app.component.html',
+  // styles: ['body: color: pink'],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
